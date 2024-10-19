b8f071d7ef3764dde06c71ab2bd27f3f0745f03a
unknown <jeanyannjulians972@gmail.com>
2024-10-19 15:34:59 +0200

    Nouveau commit modif

--- app/src/main/java/com/example/textrecognitionazureai/HomeActivity.java
+++ app/src/main/java/com/example/textrecognitionazureai/HomeActivity.java
@@ -57,20 +57,7 @@ public class HomeActivity extends AppCompatActivity {
 
         });
 
-        // Action lorsque l'on clique sur le bouton pour créer un PDF
-        createPdfButton.setOnClickListener(v -> {
-            if (capturedImage != null) {
-                try {
-                    File pdfFile = createPdfFromImage(capturedImage);
-                    openPdfFile(pdfFile);  // Ouvrir le fichier PDF après la création
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "Erreur lors de la création du PDF", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(this, "Veuillez capturer une image d'abord", Toast.LENGTH_SHORT).show();
-            }
-        });
+
     }
 
     // Gérer le retour d'image de la caméra
@@ -85,40 +72,7 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    // Méthode pour convertir l'image capturée en PDF
-    private File createPdfFromImage(Bitmap bitmap) throws IOException {
-        // Convertir l'image Bitmap en byte array
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-        byte[] byteArray = stream.toByteArray();
 
-        // Créer un fichier PDF dans le répertoire de stockage externe
-        File pdfFile = new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "ImageToPDF.pdf");
-        PdfWriter writer = new PdfWriter(new FileOutputStream(pdfFile));
-        PdfDocument pdfDocument = new PdfDocument(writer);
-        Document document = new Document(pdfDocument);
 
-        // Ajouter l'image au document PDF
-        ImageData imageData = ImageDataFactory.create(byteArray);
-        Image pdfImage = new Image(imageData);
-        document.add(pdfImage);
 
-        // Fermer le document PDF
-        document.close();
-
-        // Notifier que le PDF est créé
-        Toast.makeText(this, "PDF créé avec succès : " + pdfFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
-
-        return pdfFile;
-    }
-
-    // Méthode pour ouvrir le fichier PDF créé
-    private void openPdfFile(File pdfFile) {
-        Uri pdfUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", pdfFile);
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(pdfUri, "application/pdf");
-        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        startActivity(intent);
-    }
 }
